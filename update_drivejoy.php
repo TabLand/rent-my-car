1c6b0c1942c0e2c8443632b9113b27a395696870
Ijtaba Hussain <ijtabahussain@live.com>
2015-12-10 07:08:07 +0000

    Make script safer

--- update_drivejoy.php
+++ update_drivejoy.php
@@ -7,17 +7,22 @@
     file_put_contents("$project_root/drivejoy_dump",$drivejoy);
 
     $html = file_get_html("$project_root/drivejoy_dump");
-
-    $day_price   = $html->find('div.day   div', 0)->plaintext;
-    $week_price  = $html->find('div.week  div', 0)->plaintext;
-    $month_price = $html->find('div.month div', 0)->plaintext;
-
-    $response_rate = $html->find('div.response-rate span', 0)->plaintext;
-    $response_time = $html->find('div.response-time span', 0)->plaintext;
     
-    $pricing = "$day_price/day, $week_price/week, $month_price/month";
-    file_put_contents("$project_root/pricing",$pricing);
+    if(get_class($html) == "simple_html_dom"){
+        $day_price   = $html->find('div.day   div', 0)->plaintext;
+        $week_price  = $html->find('div.week  div', 0)->plaintext;
+        $month_price = $html->find('div.month div', 0)->plaintext;
 
-    $responsiveness =  preg_replace('/\s+/', ' ',"I have been known to respond in $response_time on average and $response_rate of the time");
-    file_put_contents("$project_root/responsiveness",$responsiveness);
+        $response_rate = $html->find('div.response-rate span', 0)->plaintext;
+        $response_time = $html->find('div.response-time span', 0)->plaintext;
+        
+        $pricing = "$day_price/day, $week_price/week, $month_price/month";
+        file_put_contents("$project_root/pricing",$pricing);
+
+        $responsiveness =  preg_replace('/\s+/', ' ',"I have been known to respond in $response_time on average and $response_rate of the time");
+        file_put_contents("$project_root/responsiveness",$responsiveness);
+    }{
+        else echo get_class($html);
+        echo "\nSomething just went royally wrong, wait till next run"
+    }
 ?>
