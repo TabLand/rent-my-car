380720bf1618795b6611bf44b0dc8e9c85c6780a
Ijtaba Hussain <ijtabahussain@live.com>
2015-12-13 10:44:38 +0000

    Stop cron complaning

--- update_drivejoy.php
+++ update_drivejoy.php
@@ -8,7 +8,7 @@
 
     $html = file_get_html("$project_root/drivejoy_dump");
     
-    if(get_class($html) == "simple_html_dom"){
+    if($html){
         $day_price   = $html->find('div.day   div', 0)->plaintext;
         $week_price  = $html->find('div.week  div', 0)->plaintext;
         $month_price = $html->find('div.month div', 0)->plaintext;
@@ -21,8 +21,7 @@
 
         $responsiveness =  preg_replace('/\s+/', ' ',"I have been known to respond in $response_time on average and $response_rate of the time");
         file_put_contents("$project_root/responsiveness",$responsiveness);
-    }{
-        else echo get_class($html);
-        echo "\nSomething just went royally wrong, wait till next run"
+    } else {
+        echo "\nSomething just went royally wrong, wait till next run";
     }
 ?>
